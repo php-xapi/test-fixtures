db3b3bb37b13e87e32778d8bb84d89255f3b5921
Christian Flothmann <christian.flothmann@xabbuh.de>
2016-08-24 09:21:06 +0200

    fix master branch alias

---
+++ src/ExtensionsFixtures.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Xabbuh\XApi\DataFixtures;
+
+use Xabbuh\XApi\Model\Extensions;
+
+/**
+ * xAPI statement extensions fixtures.
+ *
+ * These fixtures are borrowed from the
+ * {@link https://github.com/adlnet/xAPI_LRS_Test Experience API Learning Record Store Conformance Test} package.
+ */
+class ExtensionsFixtures
+{
+    public static function getTypicalExtensions()
+    {
+        return new Extensions(array('http://id.tincanapi.com/extension/topic' => 'Conformance Testing'));
+    }
+
+    public static function getWithObjectValueExtensions()
+    {
+        $color = new \stdClass();
+        $color->model = 'RGB';
+        $color->value = '#FFFFFF';
+
+        return new Extensions(array('color' => $color));
+    }
+
+    public static function getWithIntegerValueExtensions()
+    {
+        return new Extensions(array('http://id.tincanapi.com/extension/starting-position' => 1));
+    }
+
+    public static function getMultiplePairsExtensions()
+    {
+        return new Extensions(array(
+            'http://id.tincanapi.com/extension/topic' => 'Conformance Testing',
+            'http://id.tincanapi.com/extension/color' => array(
+                'model' => 'RGB',
+                'value' => '#FFFFFF',
+            ),
+            'http://id.tincanapi.com/extension/starting-position' => 1,
+        ));
+    }
+
+    public static function getInvalidNonIriExtensions()
+    {
+        return new Extensions(array('test' => 'key not an IRI'));
+    }
+}
